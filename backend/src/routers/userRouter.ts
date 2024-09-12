2e36d5b0402dce681d7f029931456082c394f8d3
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-12 13:37:13 +0400

    router for login

--- backend/src/routers/userRouter.ts
+++ backend/src/routers/userRouter.ts
@@ -5,5 +5,5 @@ import { loginUser, registerUser } from "../controllers/userController";
 const router = express.Router();
 
 router.post("/register",(req:Request,res:Response)=>registerUser(req,res))
-
+router.post("/login",(req:Request,res:Response)=>loginUser(req,res))
 export default router
