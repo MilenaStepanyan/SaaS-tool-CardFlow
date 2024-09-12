bfbfd4eafe595b730fe4f8450e4e7e18768d922b
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-12 13:12:33 +0400

    3xx status codes

--- backend/src/statusCodes.ts
+++ backend/src/statusCodes.ts
@@ -16,6 +16,15 @@ export const STATUS_CODES = {
     ALREADY_REPORTED: 208,
     IM_USED: 226,
   
-    
-  };
+    // 3xx: Redirection
+    MULTIPLE_CHOICES: 300,
+    MOVED_PERMANENTLY: 301,
+    FOUND: 302,
+    SEE_OTHER: 303,
+    NOT_MODIFIED: 304,
+    USE_PROXY: 305,
+    TEMPORARY_REDIRECT: 307,
+    PERMANENT_REDIRECT: 308,
+  
+}
   
