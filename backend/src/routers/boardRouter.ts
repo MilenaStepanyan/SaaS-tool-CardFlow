de7e37fa191ab1297450fa49cc099b78fcef3cf5
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-12 15:11:09 +0400

    boardbyid route settled

--- backend/src/routers/boardRouter.ts
+++ backend/src/routers/boardRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createBoard, getBoard } from "../controllers/boardController";
+import { createBoard, getBoard, getBoardById } from "../controllers/boardController";
 import { verifyToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.post("/create", verifyToken, createBoard);
 router.get("/get", verifyToken, getBoard);
+router.get("/:boardId", getBoardById);
 export default router
