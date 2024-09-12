1465039a1914ec0d6604ef2a5da0e702d8a779cf
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-12 12:55:04 +0400

    user Registering logic written

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -3,15 +3,22 @@ import { Request, Response } from "express";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcryptjs"
 
-export const registerUser = async(req:Request,res:Response)=>{
-    const {username,email,password} = req.body
-    if(!username||!email||!password){
-        res.status(400).json({msg:"Missing required fields"})
-    }
+export const registerUser = async(
+    req:Request,res:Response
+    ): Promise<Response> =>{
     try{
-       //
+        const {username,email,password} = req.body
+        if(!username||!email||!password){
+           return res.status(400).json({msg:"Missing required fields"})
+        }
+        const hasshedPassword = await bcrypt.hash(password,10)
+        await promisePool.query(
+            `INSERT INTO users (username,email,password) VALUES (?,?,?)`,
+            [username,email,hasshedPassword]
+        )
+        return res.status(200).json({msg:"User Registered"})
     }catch(Error){
         console.log(Error)
-        res.status(500).json({msg:"Server Error"})
+        return res.status(500).json({msg:"Server Error"})
     }
 }
