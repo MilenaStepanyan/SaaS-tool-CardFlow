39950180dc7846989dd04c044ad1ef6abed337b8
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-12 15:33:29 +0400

    createLists function settled

---
+++ backend/src/controllers/listController.ts
@@ -0,0 +1,31 @@
+import { STATUS_CODES } from "../utils/statusCodes";
+import promisePool from "../pool-connection/database";
+import { Request, Response } from "express";
+import { ResultSetHeader } from "mysql2";
+import { RowDataPacket } from "mysql2";
+import { OkPacket } from "mysql2";
+
+export const createList = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { title } = req.body;
+    const { boardId } = req.params;
+    if (!title) {
+      return res
+        .status(STATUS_CODES.BAD_REQUEST)
+        .json({ msg: "Missing required fields" });
+    }
+    const [result] = await promisePool.query<ResultSetHeader>(
+      `INSERT INTO lists (name, board_id, created_at, updated_at) VALUES (?, ?, NOW(), NOW())`,
+      [name, boardId]
+    );
+    return res.status(STATUS_CODES.CREATED).json({ listId: result.insertId });
+  } catch (Error) {
+    console.log(Error);
+    return res
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Server error" });
+  }
+};
