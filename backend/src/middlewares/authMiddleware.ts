098754f1ac9f3a08971296e68d82c65b98f248e3
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-12 13:53:57 +0400

    status codes fix

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from "express";
+import { STATUS_CODES } from "../utils/statusCodes";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
 const secret_key = process.env.JWT_SECRET;
-if(!secret_key){
-    throw new Error ('Secret Key Required')
+if (!secret_key) {
+  throw new Error("Secret Key Required");
 }
 declare global {
   namespace Express {
@@ -26,7 +27,9 @@ export const verifyToken = async (
   const token = authHeader?.replace("Bearer ", "").trim();
 
   if (!token) {
-    return res.status(401).json({ msg: "No token provided" });
+    return res
+      .status(STATUS_CODES.UNAUTHORIZED)
+      .json({ msg: "No token provided" });
   }
 
   try {
@@ -37,12 +40,18 @@ export const verifyToken = async (
     next();
   } catch (error: unknown) {
     if (error instanceof jwt.TokenExpiredError) {
-      return res.status(401).json({ msg: "Token has expired" });
+      return res
+        .status(STATUS_CODES.UNAUTHORIZED)
+        .json({ msg: "Token has expired" });
     } else if (error instanceof jwt.JsonWebTokenError) {
-      return res.status(401).json({ msg: "Token is not valid" });
+      return res
+        .status(STATUS_CODES.UNAUTHORIZED)
+        .json({ msg: "Token is not valid" });
     }
 
     console.error("Unexpected Error:", error);
-    return res.status(500).json({ msg: "Server error" });
+    return res
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Server error" });
   }
 };
