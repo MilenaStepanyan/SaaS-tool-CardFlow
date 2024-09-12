3779e1994ce20a142bdc91346fe2acbbe8307bc2
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-12 13:25:53 +0400

    login logic fully settled

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,34 +1,72 @@
 import promisePool from "../pool-connection/database";
 import { Request, Response } from "express";
-import jwt from "jsonwebtoken"
-import bcrypt from "bcryptjs"
-const SECRET_KEY = process.env.JWT_SECRET
-if(!SECRET_KEY){
-    throw new Error ('Secret Key Required')
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { STATUS_CODES } from "../utils/statusCodes";
+import { RowDataPacket } from "mysql2";
+import { User } from "../models/User";
+const SECRET_KEY = process.env.JWT_SECRET;
+if (!SECRET_KEY) {
+  throw new Error("Secret Key Required");
 }
-export const registerUser = async(
-    req:Request,res:Response
-    ): Promise<Response> =>{
-    try{
-        const {username,email,password} = req.body
-        if(!username||!email||!password){
-           return res.status(400).json({msg:"Missing required fields"})
-        }
-        const hasshedPassword = await bcrypt.hash(password,10)
-        await promisePool.query(
-            `INSERT INTO users (username,email,password) VALUES (?,?,?)`,
-            [username,email,hasshedPassword]
-        )
-        return res.status(200).json({msg:"User Registered"})
-    }catch(Error){
-        console.log(Error)
-        return res.status(500).json({msg:"Server Error"})
+export const registerUser = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res
+        .status(STATUS_CODES.BAD_REQUEST)
+        .json({ msg: "Missing required fields" });
     }
-}
-export const loginUser = async(req:Request,res:Response)=>{
-try{
-   //
-}catch{
-//
-}
-}
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await promisePool.query(
+      `INSERT INTO users (username,email,password) VALUES (?,?,?)`,
+      [username, email, hashedPassword]
+    );
+    return res.status(STATUS_CODES.OK).json({ msg: "User Registered" });
+  } catch (Error) {
+    console.log(Error);
+    return res
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Server Error" });
+  }
+};
+export const loginUser = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res
+        .status(STATUS_CODES.BAD_REQUEST)
+        .json({ msg: "Missing required files" });
+    }
+    const [rows]: [RowDataPacket[], any] = await promisePool.query(
+      `SELECT * FROM users WHERE username=?`
+    );
+    if (rows.length === 0) {
+      return res.status(STATUS_CODES.NOT_FOUND).json({ msg: "User does not exist" });
+    }
+    const user: User = rows[0] as User;
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(STATUS_CODES.BAD_REQUEST)
+        .json({ msg: "Wrong password" });
+    }
+    const token = jwt.sign({ userId: user.id }, SECRET_KEY, {
+      expiresIn: "1h",
+    });
+    console.log(user);
+
+    return res.json({ token });
+  } catch (Error) {
+    console.log(Error);
+    return res
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Server Error" });
+  }
+};
