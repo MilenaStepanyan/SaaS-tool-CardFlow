e2458beae28df2146cd511c2320092e882383d8d
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-17 13:42:48 +0400

    cardRouter attached to server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,22 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import authRouter from "./routers/userRouter"
-import boardRouter from "./routers/boardRouter"
-import listRouter from "./routers/listRouter"
+import authRouter from "./routers/userRouter";
+import boardRouter from "./routers/boardRouter";
+import listRouter from "./routers/listRouter";
+import cardRouter from "./routers/cardRouter";
 const app = express();
 const PORT = 4000;
 
-app.use(cors());  
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/status", (req: Request, res: Response) => {
   res.json({ message: "Backend is running" });
 });
-app.use("/api/user",authRouter)
-app.use("/api/board",boardRouter)
-app.use("/api",listRouter)
+app.use("/api/user", authRouter);
+app.use("/api/board", boardRouter);
+app.use("/api", listRouter);
+app.use("/api", cardRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
