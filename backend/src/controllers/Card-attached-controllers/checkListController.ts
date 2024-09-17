374fe22c60361deaaef93360675c4f2c901eaf7d
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-17 14:36:53 +0400

    skeleton for creating checklist

--- backend/src/controllers/Card-attached-controllers/checkListController.ts
+++ backend/src/controllers/Card-attached-controllers/checkListController.ts
@@ -3,3 +3,10 @@ import { STATUS_CODES } from "../../utils/statusCodes";
 import promisePool from "../../pool-connection/database";
 import { ResultSetHeader } from "mysql2";
 import { RowDataPacket } from "mysql2";
+
+export const createCard = async (
+    req: Request,
+    res: Response
+  ) => {
+    //
+  };
