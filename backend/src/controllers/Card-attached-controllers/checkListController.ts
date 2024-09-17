ef7752e2ba61f10c7675d795b985fcbf1ab3938e
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-17 14:46:16 +0400

    getAllChecklists function fully handled

--- backend/src/controllers/Card-attached-controllers/checkListController.ts
+++ backend/src/controllers/Card-attached-controllers/checkListController.ts
@@ -37,8 +37,34 @@ export const createChecklist = async (
     }
   };
   export const getAllChecklists = async (
-    req: Request,
-    res: Response
-  ) => {
-//
-  };
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { cardId } = req.params;
+    if (!cardId) {
+      return res
+        .status(STATUS_CODES.BAD_REQUEST)
+        .json({ msg: "Missing required field:cardId" });
+    }
+    const [cardExist] = await promisePool.query<RowDataPacket[]>(
+      `SELECT id FROM cards WHERE id = ?`,
+      [cardId]
+    );
+    if (cardExist.length === 0) {
+      return res
+        .status(STATUS_CODES.NOT_FOUND)
+        .json({ msg: "Card does not exist" });
+    }
+    const [rows]: [RowDataPacket[], any] = await promisePool.query(
+      `SELECT * FROM checklists WHERE card_id = ?`,
+      [cardId]
+    );
+    return res.status(STATUS_CODES.OK).json({ checklists: rows || [] });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Server Error" });
+  }
+};
