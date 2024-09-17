3b1a19060d9a5fccfe16a55c89b93dcef676fd4d
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-17 14:24:52 +0400

    commentRouter handled

---
+++ backend/src/routers/commentRouter.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { addComment, deleteComment, editComment, getAllComments } from "../controllers/CommentController";
+
+const router = Router();
+
+router.post("/cards/:cardId/comments", addComment);
+
+router.get("/cards/:cardId/comments", getAllComments);
+
+router.put("/comments/:commentId", editComment);
+
+router.delete("/comments/:commentId", deleteComment);
+
+export default router;
