0560721b916a85893fdf28f27a8bfb65da1b05c4
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-17 12:37:58 +0400

    createCard function settled

---
+++ backend/src/controllers/cardController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { STATUS_CODES } from "../utils/statusCodes";
+import promisePool from "../pool-connection/database";
+import { ResultSetHeader } from "mysql2";
+import { RowDataPacket } from "mysql2";
+import { OkPacket } from "mysql2";
+
+export const createCard = async (req:Request,res:Response)=>{
+    //
+}
