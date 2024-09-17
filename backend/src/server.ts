9b834e6bcfa7a2beac85d2ee6568275c64b3d7da
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-17 14:26:31 +0400

    commentRouter attached to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,7 @@ import authRouter from "./routers/userRouter";
 import boardRouter from "./routers/boardRouter";
 import listRouter from "./routers/listRouter";
 import cardRouter from "./routers/cardRouter";
+import commentRouter from "./routers/commentRouter";
 const app = express();
 const PORT = 4000;
 
@@ -17,6 +18,7 @@ app.use("/api/user", authRouter);
 app.use("/api/board", boardRouter);
 app.use("/api", listRouter);
 app.use("/api", cardRouter);
+app.use("/api", commentRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
