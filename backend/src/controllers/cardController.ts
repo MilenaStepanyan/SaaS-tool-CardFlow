a78f529c6461f04e7f347fbb62b4a0a4fd928222
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-17 12:47:59 +0400

    create Card function handled with extra error check for list existence

--- backend/src/controllers/cardController.ts
+++ backend/src/controllers/cardController.ts
@@ -9,16 +9,24 @@ export const createCard = async (req: Request, res: Response) => {
   try {
     const { title, description } = req.body;
     const { listId } = req.params;
-    if (!title) {
+    if (!title || !listId) {
       return res
         .status(STATUS_CODES.BAD_REQUEST)
         .json({ msg: "Missing required fields" });
     }
-    const [result] =await promisePool.query<ResultSetHeader>(
-        `INSERT INTO cards (name, description,list_id, created_at, updated_at) VALUES (?, ?, ?, NOW(), NOW())`,
-        [title,description,listId]
-    )
-    return res.status(STATUS_CODES.OK).json({cardId:result.insertId})
+    const [listExists] = await promisePool.query<RowDataPacket[]>(
+      `SELECT id FROM lists WHERE id = ?`,
+      [listId]
+    );
+
+    if (listExists.length === 0) {
+      return res.status(STATUS_CODES.NOT_FOUND).json({ msg: "List not found" });
+    }
+    const [result] = await promisePool.query<ResultSetHeader>(
+      `INSERT INTO cards (name, description,list_id, created_at, updated_at) VALUES (?, ?, ?, NOW(), NOW())`,
+      [title, description || null, listId]
+    );
+    return res.status(STATUS_CODES.OK).json({ cardId: result.insertId });
   } catch (Error) {
     console.log(Error);
     return res
