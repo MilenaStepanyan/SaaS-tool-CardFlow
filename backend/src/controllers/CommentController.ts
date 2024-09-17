90f207aa64152a4afbfe4d63b1c3a7f7b2ac42ab
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-17 13:45:46 +0400

    comment controller setting up

---
+++ backend/src/controllers/CommentController.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+import { STATUS_CODES } from "../utils/statusCodes";
+import promisePool from "../pool-connection/database";
+import { ResultSetHeader } from "mysql2";
+import { RowDataPacket } from "mysql2";
+
