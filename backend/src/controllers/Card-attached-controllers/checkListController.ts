cfa6f52a7c9b2a050fa92c71611ea012adf49d9c
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-17 14:41:12 +0400

    createChecklist handled with deep error check

--- backend/src/controllers/Card-attached-controllers/checkListController.ts
+++ backend/src/controllers/Card-attached-controllers/checkListController.ts
@@ -4,9 +4,35 @@ import promisePool from "../../pool-connection/database";
 import { ResultSetHeader } from "mysql2";
 import { RowDataPacket } from "mysql2";
 
-export const createCard = async (
+export const createChecklist = async (
     req: Request,
     res: Response
-  ) => {
-    //
+  ): Promise<Response> => {
+    try {
+      const { name } = req.body;
+      const { cardId } = req.params;
+      if (!name || !cardId) {
+        return res
+          .status(STATUS_CODES.BAD_REQUEST)
+          .json({ msg: "Missing required fields" });
+      }
+      const [cardExist] = await promisePool.query<RowDataPacket[]>(
+        `SELECT id FROM cards WHERE id = ?`,
+        [cardId]
+      );
+  
+      if (cardExist.length === 0) {
+        return res.status(STATUS_CODES.NOT_FOUND).json({ msg: "Card not found" });
+      }
+      const [result] = await promisePool.query<ResultSetHeader>(
+        `INSERT INTO checklists (name,card_id, created_at) VALUES (?, ?, NOW())`,
+        [name,cardId]
+      );
+      return res.status(STATUS_CODES.CREATED).json({ checklistId: result.insertId });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .json({ msg: "Server Error" });
+    }
   };
