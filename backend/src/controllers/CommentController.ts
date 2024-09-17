0bb74e097a73018392b458eff375b7f68f030bb8
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-17 14:12:41 +0400

    getAllComments function fully handled with deep error check

--- backend/src/controllers/CommentController.ts
+++ backend/src/controllers/CommentController.ts
@@ -14,7 +14,7 @@ export const addComment = async (req: Request, res: Response) => {
         .status(STATUS_CODES.BAD_REQUEST)
         .json({ msg: "Missing required fields" });
     }
-    const [cardExist] : [RowDataPacket[], any]= await promisePool.query(
+    const [cardExist]: [RowDataPacket[], any] = await promisePool.query(
       `SELECT id FROM cards WHERE id = ?`,
       [cardId]
     );
@@ -37,16 +37,35 @@ export const addComment = async (req: Request, res: Response) => {
   }
 };
 
-export const getAllCards = async (
-    req: Request,
-    res: Response
-  ) => {
-    try {
-     //
-    } catch (error) {
-      console.log(error);
+export const getAllComments = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { cardId } = req.params;
+    if (!cardId) {
       return res
-        .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
-        .json({ msg: "Server Error" });
+        .status(STATUS_CODES.BAD_REQUEST)
+        .json({ msg: "Missing required field card Id" });
     }
-  };
+    const [cardExist] = await promisePool.query<RowDataPacket[]>(
+      `SELECT id FROM cards WHERE id = ?`,
+      [cardId]
+    );
+    if (cardExist.length === 0) {
+      return res
+        .status(STATUS_CODES.NOT_FOUND)
+        .json({ msg: "card does not exist" });
+    }
+    const [rows]: [RowDataPacket[], any] = await promisePool.query(
+      `SELECT * FROM comments WHERE card_id = ?`,
+      [cardId]
+    );
+    return res.status(STATUS_CODES.OK).json({ comments: rows || [] });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Server Error" });
+  }
+};
