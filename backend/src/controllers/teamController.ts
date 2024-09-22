827fcba688d7683d048710d4c989a73029334737
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-22 15:32:07 +0400

    createTeam skeleton settled

--- backend/src/controllers/teamController.ts
+++ backend/src/controllers/teamController.ts
@@ -4,6 +4,14 @@ import { Request, Response } from "express";
 import { ResultSetHeader } from "mysql2";
 import { RowDataPacket } from "mysql2";
 
-export const addMember = async(req:Request,res:Response)=>{
+export const createTeam = async(Req:Request,res:Response)=>{
     //
+}
+export const addMember = async(req:Request,res:Response)=>{
+    try{
+
+    }catch(error){
+        console.log(error);
+        return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json({msg:"Server Error"})
+    }
 }
