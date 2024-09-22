6e40d2fd1448b80278f9c79e8222dc01e0f4b12f
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-22 15:29:33 +0400

    addMmber skeleton settled

--- backend/src/controllers/teamController.ts
+++ backend/src/controllers/teamController.ts
@@ -1,1 +1,9 @@
-import {Request,Response} from "express"
+import { STATUS_CODES } from "../utils/statusCodes";
+import promisePool from "../pool-connection/database";
+import { Request, Response } from "express";
+import { ResultSetHeader } from "mysql2";
+import { RowDataPacket } from "mysql2";
+
+export const addMember = async(req:Request,res:Response)=>{
+    //
+}
