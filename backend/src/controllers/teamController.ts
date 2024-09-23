18f34dd85122a0ef85b370490e568a25f53bc337
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-09-23 14:06:51 +0400

    createTeam function settled

--- backend/src/controllers/teamController.ts
+++ backend/src/controllers/teamController.ts
@@ -4,14 +4,37 @@ import { Request, Response } from "express";
 import { ResultSetHeader } from "mysql2";
 import { RowDataPacket } from "mysql2";
 
-export const createTeam = async(Req:Request,res:Response)=>{
-    //
-}
-export const addMember = async(req:Request,res:Response)=>{
-    try{
+export const createTeam = async (req: Request, res: Response) => {
+  const { name, description, ownerId } = req.body;
+  try {
+    const [result]: [ResultSetHeader, any] = await promisePool.query(
+      `INSERT INTO teams  (name,description,owner_id) VALUES (?, ?, ?)`,
+      [name, description || null, ownerId]
+    );
+    const teamId = result.insertId;
 
-    }catch(error){
-        console.log(error);
-        return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json({msg:"Server Error"})
-    }
-}
+    await promisePool.query(
+      `INSERT INTO team_members (team_id, user_id, role) VALUES (?, ?, 'admin')`,
+      [teamId, ownerId]
+    );
+
+    return res.status(STATUS_CODES.CREATED).json({
+      msg: "Team created successfully",
+      teamId: teamId,
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Server Error" });
+  }
+};
+export const addMember = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Server Error" });
+  }
+};
