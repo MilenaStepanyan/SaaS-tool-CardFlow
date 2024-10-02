6e32d59d99bc89043e2bf64cddcc95ad1e369671
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-10-03 00:12:33 +0400

    handlelogin function settled

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,18 +1,29 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export const Login:React.FC = () => {
-    const [username, setUsername] = useState<string>("")
-    const [email, setEmail] = useState<string>("")
-    const [password, setPassword] = useState<string>("")
-    const [error, setError] = useState<string | null>(null);
-    const navigate = useNavigate()
+export const Login: React.FC = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
-
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `
+            ${import.meta.env.VITE_API_URL}/user/login`,
+        { username, password }
+      );
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+      setError("An unexpected error occurred");
+    }
+  };
   return (
-        <>
-        <h1>wassup</h1>
-        </>
-  )
-}
+    <>
+     </>
+  );
+};
