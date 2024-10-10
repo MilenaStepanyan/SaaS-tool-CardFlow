98662ce01a02916f4b5ae74d50a518c3b124ff16
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-10-11 02:09:08 +0400

    css is given for both login and register page

--- frontend/src/components/Auth/Login.tsx
+++ frontend/src/components/Auth/Login.tsx
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import illustration from "../../../public/login-illustration.png";
+import iconFirst from "../../../public/trello-left.4f52d13c.svg";
+import iconSecond from "../../../public/trello-right.e6e102c7.svg";
+import google from "../../../public/google.png";
+import microsoft from "../../../public/microsoft.png";
+import apple from "../../../public/apple.png";
+import logo from "../../../public/logo.png";
 import { Header } from "../Header";
+import { Link } from "react-router-dom";
+
 export const Login: React.FC = () => {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
@@ -25,30 +32,67 @@ export const Login: React.FC = () => {
       setError("An unexpected error occurred");
     }
   };
+
   return (
     <>
-      <Header />
-      <div className="main">
-        <img className="login-illustration" src={illustration} alt="" />
-        <form onSubmit={handleLogin}>
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+      <div className="main-container">
 
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button type="submit">Login</button>
-        </form>
-        {error && <p>{error}</p>}
+        <img
+          src={iconSecond}
+          className="icons-for-signup-in right-icon"
+          alt=""
+        />
+        <div className="login-form-container">
+        <img src={logo} className="logo-w-out-writing" alt="" />
+          <form onSubmit={handleLogin} className="login-form">
+            <input
+              type="text"
+              className="login-input text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              className="login-input password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button type="submit" className="login-button">
+              Login
+            </button>
+          </form>
+          <div className="sign-up-with">
+            <h4 className="continue-with">Or continue with:</h4>
+            <div className="google">
+              <img src={google} alt="" className="google-icon" />
+              <p>Google</p>
+            </div>
+            <div className="microsoft">
+              <img src={microsoft} alt="" className="ms-icon" />
+              <p>Microsoft</p>
+            </div>
+            <div className="apple">
+              <img src={apple} alt="" className="apple-icon" />
+              <p>Apple</p>
+            </div>
+          </div>
+          <p className="login-text">
+            By signing up, I accept the Atlassian Cloud Terms of Service and
+            acknowledge the Privacy Policy.
+          </p>
+          <p className="login-text">
+            One account for Trello, Jira, Confluence and more. This site is
+            protected by reCAPTCHA and the Google Privacy Policy and Terms of
+            Service apply.
+          </p>
+          <Link className="link-to-each-page" to="/register">Sign Up</Link>
+        </div>
+        <img src={iconFirst} className="icons-for-signup-in left-icon" alt="" />
+        {error && <p className="error-message">{error}</p>}
       </div>
     </>
   );
--- frontend/src/components/Auth/Register.tsx
+++ frontend/src/components/Auth/Register.tsx
@@ -2,70 +2,93 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import google from "../../../public/google.png";
+import microsoft from "../../../public/microsoft.png";
+import apple from "../../../public/apple.png";
+import logo from "../../../public/logo.png";
+import iconFirst from "../../../public/trello-left.4f52d13c.svg";
+import iconSecond from "../../../public/trello-right.e6e102c7.svg";
+
 export const Register: React.FC = () => {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [error, setError] = useState<string | null>(null);
   const navigate = useNavigate();
+
   const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
       const response = await axios.post(
         `${import.meta.env.VITE_API_URL}/user/register`,
-        {
-          username,
-          email,
-          password,
-        }
+        { username, email, password }
       );
       navigate("/login");
-      console.log("User Registerred", response.data);
+      console.log("User Registered", response.data);
     } catch (err) {
       console.error(err);
       setError("An unexpected error occurred");
     }
   };
+
   return (
-    <div className="register-container">
-    <div className="main">
-    {error && <p style={{ color: "red" }}>{error}</p>}
-    <form onSubmit={handleRegister}>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+    <>
+      <div className="main-container">
+        <img src={iconSecond} className="icons-for-signup-in right-icon" alt="" />
+        <div className="register-form-container">
+          <img src={logo} className="logo-w-out-writing" alt="" />
+          <form onSubmit={handleRegister} className="login-form">
+            <input
+              type="text"
+              className="login-input text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <input
+              type="email"
+              className="login-input text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              className="login-input password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button type="submit" className="login-button">
+              Register
+            </button>
+          </form>
+          <div className="sign-up-with">
+            <h4 className="continue-with">Or continue with:</h4>
+            <div className="google">
+              <img src={google} alt="" className="google-icon" />
+              <p>Google</p>
+            </div>
+            <div className="microsoft">
+              <img src={microsoft} alt="" className="ms-icon" />
+              <p>Microsoft</p>
+            </div>
+            <div className="apple">
+              <img src={apple} alt="" className="apple-icon" />
+              <p>Apple</p>
+            </div>
+          </div>
+          <p className="login-text">
+            By signing up, I accept the Terms of Service and acknowledge the Privacy Policy.
+          </p>
+          <Link  className="link-to-each-page" to="/login">Sign In </Link>
+          <div className="error-message">{error && <p className="error-message">{error}</p>}</div>
+        </div>
+        <img src={iconFirst} className="icons-for-signup-in left-icon" alt="" />
       </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <div className="btn">
-        <button type="submit">Register</button>
-        <Link className="signin" to={"/login"}><p>Sign in</p></Link>
-      </div>
-       
-    </form>
-    </div>
-    
-  </div>
+    </>
   );
 };
